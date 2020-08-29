097b182d58bb185c598bfafec21c98462f3834ae
Gianfranco <gianffh@gmail.com>
2020-08-30 01:25:11 +0800

    Connect song list graphQL API with the UI

--- client/components/SongList/index.js
+++ client/components/SongList/index.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-export default function SongList() {
-  return (
-    <div>Song List</div>
-  );
+class SongList extends React.Component {
+  render() {
+    const { data } = this.props;
+    return (
+      <div>
+        <h1>Song List</h1>
+        {
+          data.loading ? (
+            <div>Loading...</div>
+          ) : data.songs.map(song => (
+            <div>{song.title}</div>
+          ))
+        }
+      </div>
+    );
+  }
 }
+
+const query = gql`
+  {
+    songs {
+      title
+    }
+  }
+`;
+
+export default graphql(query)(SongList);
