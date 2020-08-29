b2f1900d0eef358ffc145fbac8c2ebfea4dd26b2
Gianfranco <gianffh@gmail.com>
2020-08-30 01:31:39 +0800

    Add react-router and add new page add song

---
+++ client/components/AddNewSong/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AddNewSong() {
+  return <h1>Add new song</h1>
+}
+
+export default AddNewSong;
--- client/index.js
+++ client/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import {
+  HashRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 // Components
 import SongList from './components/SongList';
+import AddNewSong from './components/AddNewSong';
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <HashRouter>
+        <Switch>
+          <Route path="/song/new">
+            <AddNewSong />
+          </Route>
+          <Route path="/">
+            <SongList />
+          </Route>
+        </Switch>
+      </HashRouter>
     </ApolloProvider>
   );
 };
